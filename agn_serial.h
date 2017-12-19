14ba960643add560d39965f7f07e0412a882fd8a
Kris Kalavantavanich <krismath.K@gmail.com>
2017-12-19 22:58:00 +0700

    Added Debugging messages

--- agn_serial.h
+++ agn_serial.h
@@ -17,6 +17,15 @@ public:
 		uint8_t bytes[AGN_PACKET_SIZE];
 		AGN_PACKET_SERIALIZE(packet, bytes);
 		MASTER_SERIAL.write(bytes, AGN_PACKET_SIZE);
+		char str[200];
+		char header[30] = "Sent bytes:";
+		sprintf(str, "%s", header);
+		char * a = str + strlen(header);
+		for (int i = 0; i < AGN_PACKET_SIZE; i++) {
+			sprintf(a, " %02x", bytes[i]);
+			a += 3;
+		}
+		LOGGER_SERIAL.write(str);
 	}
 
 	void receive(struct AGN_PACKET* packet) {
